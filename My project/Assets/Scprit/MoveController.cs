310f81918f450d232541ef1ebeb5d159146cf458
gf15\User <sbsgameas@gmail.com>
2024-06-14 19:51:09 +0900

    동기화

--- My project/Assets/Scprit/MoveController.cs
+++ My project/Assets/Scprit/MoveController.cs
@@ -10,7 +10,7 @@ public class MoveController : MonoBehaviour
     CapsuleCollider2D coll;
     Animator anim;
     Vector3 moveDir;
-    float vecticalVelocity = 0f; //수직으로 떨어지는 힘
+    float verticalVelocity = 0f; //수직으로 떨어지는 힘
 
     [SerializeField] float jumpForce;
     [SerializeField] float moveSpeed;
@@ -19,6 +19,7 @@ public class MoveController : MonoBehaviour
     [SerializeField] float grondCheckLength;//이길이가 게임에서 얼마만큼의 길이로 나오는지 육안으로 보기전까지는 알수가 없음
     [SerializeField] Color colorGroundCheck;
     [SerializeField] bool isGround; //인스펙터에서 플레이어가 플랫폼 타일에 착지 했는지 체크
+    bool isJump;
 
     private void OnDrawGizmos()
     {
@@ -34,6 +35,7 @@ public class MoveController : MonoBehaviour
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
@@ -45,15 +47,20 @@ public class MoveController : MonoBehaviour
     void Update()
     {
         checkGrounded();
+
         moving();
+        jump();
+
+        checkGravity();
+
+        doAnim();
     }
 
     private void checkGrounded()
     {
-        if (gameObject.CompareTag("Player") == true) //태그는 string으로 대상의 태그를 구분
-        {
+        isGround = false;
 
-        }
+        if (verticalVelocity > 0f) return;
 
         //Layer = int로 대상의 레이어를 구분
         //Layer의 int와 공통적으로 활용하는  int 와 다름
@@ -65,11 +72,6 @@ public class MoveController : MonoBehaviour
         {
             isGround = true;
         }
-        else
-        {
-            isGround = false;
-        }
-    
     }
 
     private void moving()
@@ -82,4 +84,52 @@ public class MoveController : MonoBehaviour
         
     }
 
+    private void jump()
+    {
+        //if (isGround == true  &&  Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    rigid.AddForce(new Vector2(10, jumpForce), ForceMode2D.Impulse);//지긋이 미는 힘
+        //}
+
+        if (isGround == false)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            isJump = true;
+        }
+    }
+
+    private void checkGravity()
+    {
+        if(isGround == false)//공중에 떠있을 때
+        {
+            verticalVelocity += Physics.gravity.y * Time.deltaTime; // -9.81
+
+            if(verticalVelocity < -10)
+            {
+                verticalVelocity = -10;
+            }
+        }
+        else if(isJump == true)
+        {
+            isJump = false;
+            verticalVelocity = jumpForce;
+        }
+        else if (isGround == true)
+        {
+            verticalVelocity = 0;
+        }
+
+        
+        rigid.velocity = new Vector2(rigid.velocity.x, verticalVelocity);
+    }
+
+    private void doAnim()
+    {
+        anim.SetInteger("Horizontal", (int)moveDir.x);
+        anim.SetBool("isGround", isGround);
+    }
 }
