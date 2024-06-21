b8ad4ae70419c35a310ae0f5c63d560ed6f6ea50
gf15\User <sbsgameas@gmail.com>
2024-06-21 19:52:42 +0900

    동기화

--- My project/Assets/Script/AttackController.cs
+++ My project/Assets/Script/AttackController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AttackController : MonoBehaviour
 {
     Camera mainCam;
+    [SerializeField] Transform trsHand;
+    [SerializeField] GameObject objThrowWeapon;
+    [SerializeField] Transform trsWeapon;
+    
     private void Start()
     {
         mainCam = Camera.main; // 메인 카메라
@@ -20,5 +24,32 @@ public class AttackController : MonoBehaviour
     private void checkAim()
     {
         Vector2 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPos = transform.position;
+        Vector2 fixedPos = mouseWorldPos - playerPos; //마우스와 플레이어간의 오차값
+
+
+        // fixedPos.x > 0  or  transform.localScale.x  -1  = > right    ,   1  =>  left
+
+        //칼을 돌릴때 pivot 값 변경과
+        //게임오브젝트를 만들어서 손잡이쪽에 트랜스폼을 지정해서 칼을 자식으로 넣음
+
+        float angle = Quaternion.FromToRotation(
+           transform.localScale.x > 0 ? Vector3.left : Vector3.right, fixedPos).eulerAngles.z;
+        trsHand.rotation = Quaternion.Euler(0, 0, angle);
+
+
+    }
+
+    private void checkCreate()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            createWeapon();
+        }
+    }
+
+    private void createWeapon()
+    {
+        //GameObject go = Instantiate(objThrowWeapon , trsHand);
     }
 }
---
+++ My project/Assets/Script/CursorManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [Header("커서 이미지")]
+    [SerializeField, Tooltip("0은 디폴트 <color = red> 디폴트 </color> , 1은 <color=red>클릭</color>")]
+    Texture2D[] cursors;
+   
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            Cursor.SetCursor(cursors[1] , new Vector2(cursors[1].width*0.5f , cursors[1].height * 0.5f ), CursorMode.Auto );
+        }
+        else
+        {
+            Cursor.SetCursor(cursors[0], new Vector2(cursors[0].width * 0.5f, cursors[0].height * 0.5f), CursorMode.Auto);
+        }
+    }
+}
